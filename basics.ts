c6a5fb749bb842161197d9833585737529c14beb
Fauz Ali <fauz_sp@hotmail.com>
2022-04-06 16:51:50 +0500

    first commit

---
+++ basics.ts
@@ -0,0 +1,17 @@
+let firstName = "Edward Kennway";
+let hasLoggedIn: boolean = true;
+let myRegex = /foo/;
+const splitName: string[] = firstName.split(" ");
+
+interface Person {
+    firstName: string; lastName: string;
+}
+const myPerson: Person = {
+    firstName: "Joe",
+    lastName: "Barry",
+}
+const ids:Record<number, string> = {
+    10: 'a',
+    20: 'b',
+}
+ids[30] = 'c';
